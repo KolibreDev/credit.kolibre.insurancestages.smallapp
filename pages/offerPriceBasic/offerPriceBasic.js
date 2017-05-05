6645cdc9e8159726edb0cc7fd6c7025f53b328b2
Jingchao Zhao <zhao.jingchao@kolibre.credit>
2017-05-05 14:29:53 +0800

    201705051429

--- pages/offerPriceBasic/offerPriceBasic.js
+++ pages/offerPriceBasic/offerPriceBasic.js
@@ -1,7 +1,52 @@
+const util = require('../../utils/util');
 Page({
   data: {
+  	isTransfered: false,
+  	companyUrl: '',
+  	companyCode: '',
+  	city: '上海',
+  	startDate: util.formatTime2(new Date()),
+  	transferDate: util.formatTime2(new Date())
+  },
+  onLoad: function(options) {
+  	var imgUrl = '../../images/sic_';
+  	switch(options.company) {
+  		case 'PICC':
+  			imgUrl += 'picc.png';
+  			break;
+  		case 'CPIC':
+  			imgUrl += 'cpic.png';
+  			break
+  		case 'PAIC':
+  			imgUrl += 'paic.png';
+  			break;
+  	}
+    this.setData({
+    	companyCode: options.company,
+      	companyUrl: imgUrl
+    })
+  },
+  switchChange: function (e){
+    this.setData({
+    	isTransfered:  e.detail.value
+    })
+  },
+  bindStartDateChange: function(e) {
+	this.setData({
+    	startDate:  e.detail.value
+    })
+  },
+  bindTransferDateChange: function(e) {
+	this.setData({
+    	transferDate:  e.detail.value
+    })
   },
   kindToggle: function (e) {
+  },
+  saveInfosAndNext: function(e) {
+  	wx.navigateTo({
+	  url: '../vehicle/vehicle'
+	})
   }
 })
 
